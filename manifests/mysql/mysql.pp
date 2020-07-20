d091d63ac09971b387b7188344ca35b4c8dda937
Peter <pmelse-@pop-os.localdomain>
2020-07-20 13:56:00 -0400

    test mysql

---
+++ manifests/mysql/mysql.pp
@@ -0,0 +1,29 @@
+# Class: powerdns_admin::mysql::mysql
+#
+#
+class powerdns_admin::mysql::mysql {
+  file { '/srv/mysql':
+    ensure => 'directory',
+    owner  => 'mysql',
+    group  => 'mysql',
+    mode   => '0771',
+  }
+  $override_options = {
+    'mysqld' => {
+      'bind-address'    => "${facts}['network']",
+      'datadir'         => '/srv/mysql',
+      'log_bin'         => '/srv/mysql.bin.log',
+      'max_binlog_size' => '64424509440',
+    }
+  }
+
+  # $mysql_config = lookup( { 'name' => 'mysql' } )
+
+
+  class { '::mysql::server':
+    root_password           => $powerdns_admin::mysql_root_password,
+    remove_default_accounts => true,
+    # override_options        => $override_options
+  }
+
+}
